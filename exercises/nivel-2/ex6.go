22124ea2e83b90ef3d4ed76e955960b374b4b0cc
heijunior_meli <heinzstr.junior@mercadolivre.com>
2026-05-06 09:44:21 -0300

    extra exercice dor this leacture

---
+++ exercises/nivel-2/ex6.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const (
+	_ = 1994 + iota
+	b
+	c
+	d
+	e
+)
+
+func main() {
+	fmt.Println(b, c, d, e)
+}
+
+/*Utilizando iota, crie 4 constantes cujos valores sejam os próximos 4 anos.
+Demonstre estes valores.*/
